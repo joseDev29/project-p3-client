6fd200a68e1dc0df21a064f0cbd77d66b0d5b6df
Santiago Salazar <santiago.1701913303@ucaldas.edu.co>
2021-04-16 00:51:04 -0500

    solucion 4

--- src/app/modules/parameters/parameters.module.ts
+++ src/app/modules/parameters/parameters.module.ts
@@ -1,29 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-<<<<<<< HEAD
-=======
 import { CreateCityComponent } from './city/create-city/create-city.component';
 import { UpdateCityComponent } from './city/update-city/update-city.component';
 import { ListCityComponent } from './city/list-city/list-city.component';
->>>>>>> d9e5f77d4e4b2ece64e2955c4f4343835b980576
 import { CreateCountryComponent } from './country/create-country/create-country.component';
 import { ListCountryComponent } from './country/list-country/list-country.component';
-<<<<<<< HEAD
-import { CreateCityComponent } from './city/create-city/create-city.component';
-import { UpdateCityComponent } from './city/update-city/update-city.component';
-import { ListCityComponent } from './city/list-city/list-city.component';
-
-
-
-@NgModule({
-  declarations: [
-    CreateCountryComponent,
-    UpdateCountryComponent,
-    ListCountryComponent,
-    CreateCityComponent,
-    UpdateCityComponent,
-    ListCityComponent
-=======
 import { UpdateCountryComponent } from './country/update-country/update-country.component';
 
 @NgModule({
@@ -34,7 +15,6 @@ import { UpdateCountryComponent } from './country/update-country/update-country.
     CreateCountryComponent,
     UpdateCountryComponent,
     ListCountryComponent,
->>>>>>> d9e5f77d4e4b2ece64e2955c4f4343835b980576
   ],
   imports: [CommonModule],
 })
