18f0b12ba2255b8c5d77efbf4a4634e3e6925503
Santiago Salazar <santiago.1701913303@ucaldas.edu.co>
2021-04-16 00:01:28 -0500

    Public modules created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MasterPageComponent } from './public/master-page/master-page.component';
+import { FooterComponent } from './public/shared-components/footer/footer.component';
+import { NavbarComponent } from './public/shared-components/navbar/navbar.component';
+import { SidebarComponent } from './public/shared-components/sidebar/sidebar.component';
+import { CardComponent } from './public/shared-components/card/card.component';
+import { HomeHeaderComponent } from './public/shared-components/home-header/home-header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MasterPageComponent,
+    FooterComponent,
+    NavbarComponent,
+    SidebarComponent,
+    CardComponent,
+    HomeHeaderComponent
   ],
   imports: [
     BrowserModule,
