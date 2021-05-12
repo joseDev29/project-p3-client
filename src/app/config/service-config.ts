67d8708f9dc68c2da5e234fe08738440ab97b643
Jose Miguel Ortiz C <jose.1701911606@ucaldas.edu.co>
2021-05-12 16:55:22 -0500

    cambios en el login y en el baseUrl

--- src/app/config/service-config.ts
+++ src/app/config/service-config.ts
@@ -1,5 +1,5 @@
 export namespace ServiceConfig {
-    export const BASE_URL = 'http://localhost:3000/';
-    export const normalUserRol = 1;
-    export const adminUserRol = 2;
+  export const BASE_URL = 'http://projectp3back.herokuapp.com/';
+  export const normalUserRol = 1;
+  export const adminUserRol = 2;
 }
